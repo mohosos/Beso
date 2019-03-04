54832211fd39c4bb0986f379c9d503e63b875b7e
Mohamed Ossama <mohamed.ossama@Intellisec-me.com>
2019-03-04 11:41:12 +0200

    first

---
+++ src/Base.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.AppiumDriver;
+
+public class Base {
+
+	public static AppiumDriver<WebElement> driver  ;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 
+			
+			
+		}
+	public void depedencies () {
+
+		
+	}
+
+}
